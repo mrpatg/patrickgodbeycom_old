c1da254a64ffb4f6d80cbc637de9522196c613e6
Patrick Godbey <mrpatg@gmail.com>
2019-01-17 16:15:14 -0500

    There is an error, but I don't know what it is yet.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from "gatsby"
-
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import './all.sass'
 
@@ -38,6 +38,11 @@ const TemplateWrapper = ({ children }) => (
         </Helmet>
         <Navbar />
         <div>{children}</div>
+      </div>
+      <div>
+        <Footer>
+          
+        </Footer>
       </div>
     )}
   />
