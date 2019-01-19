fa2d50ee2487827346a944cd8a7e505c990702c1
Patrick Godbey <mrpatg@gmail.com>
2019-01-19 08:50:08 -0500

    navbar markup changes on desktop

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -75,37 +75,16 @@ const Navbar = class extends React.Component {
       </div>
       <div className="is-hidden-touch">
         <aside class="menu">
-          <p class="menu-label">
-            General
-          </p>
           <ul class="menu-list">
-            <li><a>Dashboard</a></li>
-            <li><a>Customers</a></li>
-          </ul>
-          <p class="menu-label">
-            Administration
-          </p>
-          <ul class="menu-list">
-            <li><a>Team Settings</a></li>
-            <li>
-              <a class="is-active">Manage Your Team</a>
-              <ul>
-                <li><a>Members</a></li>
-                <li><a>Plugins</a></li>
-                <li><a>Add a member</a></li>
-              </ul>
-            </li>
-            <li><a>Invitations</a></li>
-            <li><a>Cloud Storage Environment Settings</a></li>
-            <li><a>Authentication</a></li>
-          </ul>
-          <p class="menu-label">
-            Transactions
-          </p>
-          <ul class="menu-list">
-            <li><a>Payments</a></li>
-            <li><a>Transfers</a></li>
-            <li><a>Balance</a></li>
+              <Link className="navbar-item" to="/about">
+                About
+              </Link>
+              <Link className="navbar-item" to="/repo">
+                Repo
+              </Link>
+              <Link className="navbar-item" to="/contact">
+                Contact
+              </Link>
           </ul>
         </aside>
       </div>
