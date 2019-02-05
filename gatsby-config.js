fb4b9e5a5b3be45d3ad7cb439e7163b853c6f1c3
Patrick Godbey <mrpatg@gmail.com>
2019-02-04 21:32:12 -0500

    added gatsby-favicon and favicon file

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,5 +66,6 @@ module.exports = {
     },
     'gatsby-plugin-purgecss', // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
+    'gatsby-plugin-favicon',
   ],
 }
